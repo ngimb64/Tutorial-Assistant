62a2c646f853d9dbef7b7fa7b126570b7e2f054e
ngimb64 <56981761+ngimb64@users.noreply.github.com>
2020-08-15 12:20:56 -0400

    Update tutuorialAssistant.py

--- tutuorialAssistant.py
+++ tutuorialAssistant.py
@@ -7,36 +7,6 @@ import logging
 import time
 import re
 
-def regex_formating(file_path):
-    regex = re.compile(r'(?:(^\[)|([\',])|(\]$))')
-    regex2 = re.compile(r'''<Key\.
-                            (?:ctrl|shift|alt|caps_lock|
-                            tab|cmd|home|insert|delete|
-                            end|page|left|down|right|up|
-                            print_screen
-                            )
-                            ((?:_l|_r|_up|_down
-                            )?)
-                            :.<[0-9]+>>''', re.X)
-
-    regex3 = re.compile(r'[^\S\r\n]')
-    regex4 = re.compile(r'.<Key\.backspace:<8>>')
-    regex5 = re.compile(r'<Key\.space:>')
-    draft_log = open(file_path + 'keys.txt')
-    final_log = open(file_path + 'commands.txt', 'a')
-
-    with draft_log as x:
-        for line in x:
-            sub = re.sub(regex, r'', str(line))
-            sub2 = re.sub(regex2, r'', str(sub))
-            sub3 = re.sub(regex3, r'', str(sub2))
-            sub4 = re.sub(regex4, r'', str(sub3))
-            result = re.sub(regex5, r' ', str(sub4))
-            final_log.write(result)
-
-    draft_log.close()
-    final_log.close()
-
 def on_press(key):
     global command_file, keys, screenshot
 
@@ -56,19 +26,50 @@ def screenshots(file_path):
         pic.save(file_path + str(random.randrange(1,500)) + '.png')
         time.sleep(5)           
 
+def regex_formating(file_path):
+    regex = re.compile(r'(?:(^\[)|([\',])|(\]$))')
+    regex2 = re.compile(r'''<Key\.
+                            (?:ctrl|shift|alt|caps_lock|
+                            tab|cmd|home|insert|delete|
+                            end|page|left|down|right|up|
+                            print_screen
+                            )
+                            ((?:_l|_r|_up|_down
+                            )?)
+                            :.<[0-9]+>>''', re.X)
+
+    regex3 = re.compile(r'[^\S\r\n]')
+    regex4 = re.compile(r'.<Key\backspace:<8>>')
+    regex5 = re.compile(r'<Key\.space:>')
+
+    draft_log = open(file_path + 'keys.txt')
+    final_log = open(file_path + 'commands.txt', 'a')
+
+    with draft_log as x:
+        for line in x:
+            sub = re.sub(regex, r'', str(line))
+            sub2 = re.sub(regex2, r'', str(sub))
+            sub3 = re.sub(regex3, r'', str(sub2))
+            sub4 = re.sub(regex4, r'', str(sub3))
+            result = re.sub(regex5, r' ', str(sub4))
+            final_log.write(result)
+
+    draft_log.close()
+    final_log.close()
+
 def main():
     global command_file, screenshot
 
     input('Please hit enter to begin\n')
     command_file = open(file_path + 'keys.txt', 'a')
 
-    key_listener = Listener(on_press=on_press) ; key_listener.start()
-    screenshot = Process(target=screenshots, args=(file_path,)) ; screenshot.start()
-    key_listener.join(600)
+    key_listener = Listener(on_press=on_press)
+    screenshot = Process(target=screenshots, args=(file_path,))
+    key_listener.start()
+    screenshot.start()
+    
+    key_listener.join(600.0)
     screenshot.join(timeout=600)
-
-    if screenshot is not None:
-        screenshot.terminate()
 
     command_file.close()
     regex_formating(file_path)
@@ -80,7 +81,7 @@ if __name__ == '__main__':
 
     try:
         pathlib.Path('C:/Users/Public/Tutorial')\
-                .mkdir(parents=True, exist_ok=True)
+                .mkdir(parents=True, exst_ok=True)
         
         file_path = 'C:\\Users\\Public\\Tutorial\\'
         keys = []
